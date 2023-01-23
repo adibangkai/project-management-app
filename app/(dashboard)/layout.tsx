787c576ed64326c76e656aa3f6cb575455c06d1c
adi bangkit <adibangkai@gmail.com>
2023-01-23 14:08:07 +0700

    layouts

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,10 +1,11 @@
+import "@/styles/global.css";
 import GlassPane from "@/components/GlassPane";
 
 export default function DashboardRootLayout({ children }) {
   return (
     <html>
       <head />
-      <body className="h-screen w-screen bg-stone-700 p-6">
+      <body className="h-screen w-screen bg-sky-200 p-6">
         <GlassPane className={"w-full h-full flex items-center justify-center"}>
           {children}
         </GlassPane>
