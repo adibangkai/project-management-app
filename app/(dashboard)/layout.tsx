3b7286a25043ae4051a57d22b3fcd88101598fa2
adi bangkit <adibangkai@gmail.com>
2023-01-25 18:56:18 +0700

    few componnets

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -5,7 +5,7 @@ export default function DashboardRootLayout({ children }) {
   return (
     <html>
       <head />
-      <body className="h-screen w-screen bg-sky-200 p-6">
+      <body className="h-screen w-screen bg-sky-900 p-6">
         <GlassPane className={"w-full h-full flex items-center justify-center"}>
           {children}
         </GlassPane>
